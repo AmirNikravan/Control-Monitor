c3cde388d82992425992ac73529f9b402ee0fec6
Amir <amirnikravan@outlook.com>
2024-09-22 15:53:31 +0330

    1. changed arduino handler

--- arduino.py
+++ arduino.py
@@ -21,23 +21,23 @@ class ArduinoHandler(QObject):
             # self.list.addItem(f"Error connecting to Arduino: {e}")
             print(f"Error connecting to Arduino: {e}")
 
-    def _send(self, data):
-        try:
-            if data == None:
-                data = '3'
-                self.serial_port.write(data)
-                print('injam')
-                print(f"Sent: {data}")
-            else:
-                self.serial_port.write(data.encode())
-            # self.list.addItem(f"Sent: {data.strip()}")
-                print(f"Sent: {data.strip()}")
-        except Exception as e:
-            # self.list.addItem(f"Error sending data: {e}")
-            print(f"Error sending data: {e}")
+    # def _send(self, data):
+    #     try:
+    #         if data == None:
+    #             data = '3'
+    #             self.serial_port.write(data)
+    #             print('injam')
+    #             print(f"Sent: {data}")
+    #         else:
+    #             self.serial_port.write(data.encode())
+    #         # self.list.addItem(f"Sent: {data.strip()}")
+    #             print(f"Sent: {data.strip()}")
+    #     except Exception as e:
+    #         # self.list.addItem(f"Error sending data: {e}")
+    #         print(f"Error sending data: {e}")
 
-    def close(self):
-        if self.serial_port and self.serial_port.is_open:
-            self.serial_port.close()
-            # self.list.addItem("Serial port closed")
-            print("Serial port closed")
+    # def close(self):
+    #     if self.serial_port and self.serial_port.is_open:
+    #         self.serial_port.close()
+    #         # self.list.addItem("Serial port closed")
+    #         print("Serial port closed")
